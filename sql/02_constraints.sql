ebcbf40d32608ff33d5a0e8e6c2a83441045d660
toma2ma2 <zzhan531@uottawa.ca>
2026-04-08 19:59:47 -0400

    Update 02_constraints.sql

--- sql/02_constraints.sql
+++ sql/02_constraints.sql
@@ -1,3 +1,5 @@
+CREATE EXTENSION IF NOT EXISTS btree_gist;
+
 ALTER TABLE hotel
     ADD CONSTRAINT chk_hotel_rating_stars
     CHECK (hotel_rating BETWEEN 1 AND 5);
@@ -12,3 +14,24 @@ ALTER TABLE room
 ALTER TABLE booking
     ADD CONSTRAINT chk_booking_not_in_past
     CHECK (start_date >= booking_date);
+
+ALTER TABLE room
+    ADD CONSTRAINT chk_room_price_range
+    CHECK (price BETWEEN 1 AND 99999);
+
+ALTER TABLE hotel
+    ADD CONSTRAINT chk_hotel_email
+    CHECK (contact_email LIKE '%@%.%');
+
+ALTER TABLE employee
+    ADD CONSTRAINT chk_ssn_format
+    CHECK (ssn_sin LIKE 'SSN-%');
+
+ALTER TABLE booking
+    ADD CONSTRAINT chk_no_overlapping_bookings
+    EXCLUDE USING gist (
+        hotel_id WITH =,
+        room_number WITH =,
+        daterange(start_date, end_date) WITH &&
+    )
+    WHERE (status = 'active');
